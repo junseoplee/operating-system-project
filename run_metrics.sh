19b77923aee2f2ac33fcec164f6b5fddf436b767
junseoplee <junseoplee@soongsil.ac.kr>
2025-05-06 21:31:11 +0900

    feat: 동기화 방식에 따른 성능 및 차이 분석을 위한 스크립트 작성 (#15)

--- run_metrics.sh
+++ run_metrics.sh
@@ -2,26 +2,37 @@
 
 INPUT=$1
 OUTPUT=$2
-NUM_PROCS=$3
-NUM_THREADS=$4
+NUM_PROC=$3
+NUM_THREAD=$4
+MODE=$5
 
 if [ ! -f "$INPUT" ]; then
-  echo "입력 파일 $INPUT 이(가) 존재하지 않습니다."
+  echo "입력 파일 $INPUT 이 존재하지 않습니다."
   exit 1
 fi
 
-if [ -z "$NUM_PROCS" ] || [ -z "$NUM_THREADS" ]; then
-  echo "사용법: $0 <입력파일> <출력파일> <프로세스 수> <스레드 수>"
+if [ -z "$NUM_PROC" ] || [ -z "$NUM_THREAD" ]; then
+  echo "사용법: $0 <입력파일> <출력파일> <프로세스 수> <스레드 수> [mutex|semaphore]"
   exit 1
 fi
 
-echo -e "\n1. 프로그램 실행 + 내부 측정 결과"
-./compressor "$INPUT" "$OUTPUT" "$NUM_PROCS" "$NUM_THREADS"
+if [ "$MODE" == "semaphore" ]; then
+  BINARY="./compressor_semaphore"
+elif [ "$MODE" == "mutex" ]; then
+  BINARY="./compressor_mutex"
+else
+  BINARY="./compressor"  # 기본 버전 (동기화 없음 또는 자유 실험용)
+fi
 
-echo -e "\n2. 시스템콜 측정 결과"
+echo ""
+echo "1. 프로그램 실행 + 내부 측정 결과"
+$BINARY "$INPUT" "$OUTPUT" "$NUM_PROC" "$NUM_THREAD"
+
+echo ""
+echo "2. 시스템콜 측정 결과"
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   echo "(Linux 환경에서 strace 사용)"
-  strace -c ./compressor "$INPUT" "$OUTPUT" "$NUM_PROCS" "$NUM_THREADS"
+  strace -c $BINARY "$INPUT" "$OUTPUT" "$NUM_PROC" "$NUM_THREAD"
 else
   echo "시스템콜 분석은 현재 OS에서 지원되지 않습니다."
 fi
