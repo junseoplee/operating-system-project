a258af09f7fc484999d40e2bba33829f1e69f242
junseoplee <junseoplee@soongsil.ac.kr>
2025-05-06 20:32:45 +0900

    feat: 다중 프로세스, 다중 스레드 혼합 구조 실행을 위한 스크립트 작성 (#13)

--- run_metrics.sh
+++ run_metrics.sh
@@ -2,38 +2,26 @@
 
 INPUT=$1
 OUTPUT=$2
-NUM_WORKERS=$3
-MODE=$4  # optional: thread or process
-DEFAULT_MODE="process"
-
-# 기본 실행 대상
-BINARY="./compressor"
-
-# 스레드 모드라면 실행 파일 변경
-if [ "$MODE" == "thread" ]; then
-  BINARY="./compressor_threaded"
-elif [ "$MODE" != "" ] && [ "$MODE" != "$DEFAULT_MODE" ]; then
-  echo "❌ 잘못된 실행 모드: $MODE (허용: process, thread)"
-  exit 1
-fi
+NUM_PROCS=$3
+NUM_THREADS=$4
 
 if [ ! -f "$INPUT" ]; then
-  echo "❌ 입력 파일 $INPUT 이(가) 존재하지 않습니다."
+  echo "입력 파일 $INPUT 이(가) 존재하지 않습니다."
   exit 1
 fi
 
-if [ -z "$NUM_WORKERS" ]; then
-  echo "❌ 사용법: $0 <입력파일> <출력파일> <프로세스/스레드 수> [process|thread]"
+if [ -z "$NUM_PROCS" ] || [ -z "$NUM_THREADS" ]; then
+  echo "사용법: $0 <입력파일> <출력파일> <프로세스 수> <스레드 수>"
   exit 1
 fi
 
-echo -e "\n▶ 1. 프로그램 실행 + 내부 측정 결과"
-$BINARY "$INPUT" "$OUTPUT" "$NUM_WORKERS"
+echo -e "\n1. 프로그램 실행 + 내부 측정 결과"
+./compressor "$INPUT" "$OUTPUT" "$NUM_PROCS" "$NUM_THREADS"
 
-echo -e "\n▶ 2. 시스템콜 측정 결과"
+echo -e "\n2. 시스템콜 측정 결과"
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   echo "(Linux 환경에서 strace 사용)"
-  strace -c $BINARY "$INPUT" "$OUTPUT" "$NUM_WORKERS"
+  strace -c ./compressor "$INPUT" "$OUTPUT" "$NUM_PROCS" "$NUM_THREADS"
 else
-  echo "⚠️ 시스템콜 분석은 현재 OS에서 지원되지 않습니다."
+  echo "시스템콜 분석은 현재 OS에서 지원되지 않습니다."
 fi
